803b5f3bdf0a3a40c183cc5e18c1b75587627696
Fabian Enos <fabian.enos@gmail.com>
2018-06-07 21:33:12 +0530

    Initial commit with basic design done.

---
+++ public/js/magic.js
@@ -0,0 +1,9 @@
+angular.module("processStreetInterviewApplication", [])
+
+.component("loader", {
+    templateUrl: "/js/components/loader/template.html"
+})
+
+.component("wistiaVideo", {
+    templateUrl: "/js/components/wistia-video/template.html"
+});
